41611cd538eead7668132f12c825648fc459f9c5
KZE-THONG <155614637+KZE-THONG@users.noreply.github.com>
2025-03-24 10:42:29 +0700

    BLA-203– Refactor your project structure

--- lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:logger/web.dart';
 import 'package:provider/provider.dart';
-import 'package:week_3_blabla_project/provider/ride_pref_provider.dart';
+import 'package:week_3_blabla_project/ui/provider/ride_pref_provider.dart';
 
 import '../../../../model/location/locations.dart';
 import '../../../../model/ride/ride_pref.dart';
